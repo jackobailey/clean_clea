59f20ff67f6131c901b3262d039d15822f943bda
Jack Bailey <37049065+jackobailey@users.noreply.github.com>
2026-04-24 15:10:32 +0100

    Update Belgium and Dominican Republic corrections

--- _scripts/100_error_scripts/127.R
+++ _scripts/100_error_scripts/127.R
@@ -5,10 +5,16 @@
 #
 # https://resultatselection.belgium.be/fr/election-results/chambre-des-représentants/1929/arrondissement/163185
 
-# In cst == 20 (nivelles), the parti libéral (pty == 14) won a seat
+# In cst == 20 (nivelles), the parti libéral (pty == 14) won a seat and the
+# magnitude was 5
 
 dta$seat[
   dta$id == 127 &
     dta$cst == 20 &
     dta$pty == 14
 ] <- 1
+
+dta$mag[
+  dta$id == 127 &
+    dta$cst == 20
+] <- 5
--- _scripts/100_error_scripts/30.R
+++ _scripts/100_error_scripts/30.R
@@ -1,9 +1,25 @@
 
 # Dominican Republic (July, 1966)
 
+# Data from:
+#
+# https://resultadoselectorales.jce.gob.do/boletines/1966/nivel1966.htm
+
 # As a national constituency, I will code this as 900+ for my own purposes
 
 dta$cst[
   dta$id == 30 &
     dta$cst == 1
 ] <- 901
+
+
+# The data include a single national constituency. However, the data appear to
+# suggest that seat allocation happened at the province level and not within a
+# single national district. This may explain why the aggregate results also
+# violate the logical requirement that no party have a vote share greater than
+# the threshold of exclusion (1 / [m + 1]) without winning a seat. Since I can't
+# find seat data for this election, I will remove it from the data.
+
+dta <-
+  dta |>
+  filter(id != 30)
