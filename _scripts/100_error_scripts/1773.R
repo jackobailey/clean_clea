d6973a09404bf54244ed7d01a75325d2efbed444
Jack Bailey <37049065+jackobailey@users.noreply.github.com>
2026-05-29 13:54:35 +0100

    Add UK seat corrections

--- _scripts/100_error_scripts/1773.R
+++ _scripts/100_error_scripts/1773.R
@@ -5,6 +5,8 @@
 #
 # https://members.parliament.uk/member/4676/electionresult
 # https://en.wikipedia.org/wiki/Battersea_(UK_Parliament_constituency)
+# https://en.wikipedia.org/wiki/Cowdenbeath_and_Kirkcaldy
+# https://en.wikipedia.org/wiki/Stirling_(UK_Parliament_constituency)
 
 # In cst == 31 (Battersea), Labour won the seat
 
@@ -18,4 +20,34 @@ dta$seat[
   dta$id == 1773 &
     dta$cst == 31 &
     dta$pty == 56
+] <- 1
+
+
+# In cst == 315 (Kirkcaldy and Cowdenbeath), Labour won the seat
+
+dta$seat[
+  dta$id == 1773 &
+    dta$cst == 315 &
+    dta$pty == 121
+] <- 0
+
+dta$seat[
+  dta$id == 1773 &
+    dta$cst == 315 &
+    dta$pty == 56
+] <- 1
+
+
+# In cst == 543 (Stirling), the Conservatives won the seat
+
+dta$seat[
+  dta$id == 1773 &
+    dta$cst == 543 &
+    dta$pty == 121
+] <- 0
+
+dta$seat[
+  dta$id == 1773 &
+    dta$cst == 543 &
+    dta$pty == 27
 ] <- 1
